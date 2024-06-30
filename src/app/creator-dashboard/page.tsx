a4b622b5607f684cdee31301879814b77b9efbe9
Manice18 <manishmanice2003@gmail.com>
2024-06-30 06:47:46 +0530

    remove: Profile

--- src/app/creator-dashboard/page.tsx
+++ src/app/creator-dashboard/page.tsx
@@ -1,6 +1,4 @@
-// import Profile from "./_components/profile";
 import Events from "./_components/events";
-import DialogflowMessenger from "./_components/DialogflowMessenger";
 
 const Page = () => {
   return (
@@ -8,9 +6,7 @@ const Page = () => {
       <h1 className="ms-8 mt-24 flex text-2xl font-bold text-black">
         Dashboard
       </h1>
-      <Profile />
-      <DialogflowMessenger />
-      {/* <Events /> */}
+      <Events />
     </div>
   );
 };
